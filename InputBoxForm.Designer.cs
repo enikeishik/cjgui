35898979ea61ac3fbcb3f5126047a8e10367cb77
enikeishik <enikeishik@gmail.com>
2019-08-12 10:53:25 +0300

    feat: InputBoxForm

---
+++ InputBoxForm.Designer.cs
@@ -0,0 +1,99 @@
+﻿/*
+ * CJGui
+ * Composer JSON file GUI
+ *
+ * Created by SharpDevelop.
+ * User: Enikeishik
+ * Date: 12.08.2019
+ * Time: 10:45
+ * 
+ * @copyright   Copyright (C) 2018 Enikeishik <enikeishik@gmail.com>. All rights reserved.
+ * @author      Enikeishik <enikeishik@gmail.com>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+namespace CJGui
+{
+    partial class InputBoxForm
+    {
+        /// <summary>
+        /// Designer variable used to keep track of non-visual components.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox text;
+        private System.Windows.Forms.Button ok;
+        private System.Windows.Forms.Button cancel;
+        
+        /// <summary>
+        /// Disposes resources used by the form.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing) {
+                if (components != null) {
+                    components.Dispose();
+                }
+            }
+            base.Dispose(disposing);
+        }
+        
+        /// <summary>
+        /// This method is required for Windows Forms designer support.
+        /// Do not change the method contents inside the source code editor. The Forms designer might
+        /// not be able to load this method if it was changed manually.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.text = new System.Windows.Forms.TextBox();
+            this.ok = new System.Windows.Forms.Button();
+            this.cancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // text
+            // 
+            this.text.Location = new System.Drawing.Point(12, 12);
+            this.text.Name = "text";
+            this.text.Size = new System.Drawing.Size(260, 20);
+            this.text.TabIndex = 0;
+            // 
+            // ok
+            // 
+            this.ok.Location = new System.Drawing.Point(68, 46);
+            this.ok.Name = "ok";
+            this.ok.Size = new System.Drawing.Size(75, 23);
+            this.ok.TabIndex = 1;
+            this.ok.Text = "OK";
+            this.ok.UseVisualStyleBackColor = true;
+            this.ok.Click += new System.EventHandler(this.OkClick);
+            // 
+            // cancel
+            // 
+            this.cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancel.Location = new System.Drawing.Point(149, 46);
+            this.cancel.Name = "cancel";
+            this.cancel.Size = new System.Drawing.Size(75, 23);
+            this.cancel.TabIndex = 2;
+            this.cancel.Text = "Cancel";
+            this.cancel.UseVisualStyleBackColor = true;
+            this.cancel.Click += new System.EventHandler(this.CancelClick);
+            // 
+            // InputBoxForm
+            // 
+            this.AcceptButton = this.ok;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.cancel;
+            this.ClientSize = new System.Drawing.Size(284, 81);
+            this.ControlBox = false;
+            this.Controls.Add(this.cancel);
+            this.Controls.Add(this.ok);
+            this.Controls.Add(this.text);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "InputBoxForm";
+            this.Text = "InputBoxForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
---
+++ InputBoxForm.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * CJGui
+ * Composer JSON file GUI
+ *
+ * Created by SharpDevelop.
+ * User: Enikeishik
+ * Date: 12.08.2019
+ * Time: 10:45
+ * 
+ * @copyright   Copyright (C) 2018 Enikeishik <enikeishik@gmail.com>. All rights reserved.
+ * @author      Enikeishik <enikeishik@gmail.com>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CJGui
+{
+    /// <summary>
+    /// Description of InputBoxForm.
+    /// </summary>
+    public partial class InputBoxForm : Form
+    {
+        public InputBoxForm()
+        {
+            //
+            // The InitializeComponent() call is required for Windows Forms designer support.
+            //
+            InitializeComponent();
+        }
+        
+        void CancelClick(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+        
+        void OkClick(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+    }
+}
