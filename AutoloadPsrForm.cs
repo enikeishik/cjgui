c8f24e8cb113c97d6580321317d3f99f1d49552f
enikeishik <enikeishik@gmail.com>
2019-09-06 15:32:34 +0300

    chore: code styling

--- AutoloadPsrForm.cs
+++ AutoloadPsrForm.cs
@@ -40,16 +40,15 @@ namespace CJGui
         void AddClick(object sender, EventArgs e)
         {
             var ret = Interaction.InputBox("", "Add new value");
-            if (ret != "") {
+            if (ret != "")
                 values.Items.Add(ret);
-            }
         }
         
         void EditClick(object sender, EventArgs e)
         {
-            if (values.SelectedItems.Count == 0) {
+            if (values.SelectedItems.Count == 0)
                 return;
-            }
+            
             var val = values.SelectedItem.ToString();
             var ret = Interaction.InputBox(val, "Edit value", val);
             values.Items.Insert(values.SelectedIndex, ret);
@@ -58,9 +57,9 @@ namespace CJGui
         
         void RemoveClick(object sender, EventArgs e)
         {
-            if (values.SelectedItems.Count == 0) {
+            if (values.SelectedItems.Count == 0)
                 return;
-            }
+            
             values.Items.Remove(values.SelectedItems[0]);
         }
         
@@ -73,23 +72,23 @@ namespace CJGui
         {
             psrItem.Clear();
             var list = new List<string>(values.Items.Count);
-            foreach (var item in values.Items) {
+            
+            foreach (var item in values.Items)
                 list.Add(item.ToString());
-            }
+            
             psrItem.Add(key.Text, new StrArr(list));
         }
         
         void ThisFormLoad(object sender, EventArgs e)
         {
-            if (psrItem.Count == 0) {
+            if (psrItem.Count == 0)
                 return;
-            }
             
             key.Text = psrItem.Keys.First();
             var vals = psrItem.Values.ToList();
-            foreach (var v in vals) {
+            
+            foreach (var v in vals)
                 values.Items.Add(v.ToString().Trim(new [] {'"'}));
-            }
         }
     }
 }
